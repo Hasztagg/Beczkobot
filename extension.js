cd0bceecd81eeb59c669ac78f4d4ce52d6e1e617
Hasztagg <a.jurkiewicz5@gmail.com>
2015-08-02 13:15:14 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -99,7 +99,7 @@
         motdEnabled: false,
         motdInterval: 5,
         motd: "Zapoznaj si\u0119 z regulaminem (http:\/\/tnij.org/regulamin-beczki) i mi\u0142ej zabawy!",
-        filterChat: true,
+        filterChat: false,
         etaRestriction: false,
         welcome: true,
         opLink: null,
