4d923ba56928f5694ff681ec92be9cc3d75bdb91
Hasztagg <a.jurkiewicz5@gmail.com>
2015-08-02 14:07:50 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -109,7 +109,7 @@
         youtubeLink: null,
         website: "https://www.facebook.com/groups/beczkownicyofficial/",
         intervalMessages: ["RADA: jeżeli roz\u0142\u0105czysz si\u0119 przez przypadek, u\u017cyj !dc aby wr\u00f3ci\u0107 na swoje miejsce w kolejce.","RADA: U\u017cyj !rules aby otrzyma\u0107 link do zasad.", "RADA: Wpisz !fb albo !website aby otrzyma\u0107 link do naszej grupy na facebooku."],
-        messageInterval: 1,
+        messageInterval: 5,
         songstats: false,
         commandLiteral: "!",
         blacklists: {
