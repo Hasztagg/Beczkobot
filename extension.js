0841e4ddac537ac62692f980cea4e9dc8601cc68
Hasztagg <a.jurkiewicz5@gmail.com>
2015-08-03 14:38:33 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -56,7 +56,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me Jeżeli chcesz uzyskać awans kliknij tutaj: http://bitly.com/1e1TmUP");
+                    API.sendChat("/me Je\u017celi chcesz uzyska\u0107 awans kliknij tutaj: http://bitly.com/1e1TmUP");
                 }
             }
         };
