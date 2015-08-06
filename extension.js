5c2478d5d181ef0f29f77f12391823085deeab5c
Hasztagg <a.jurkiewicz5@gmail.com>
2015-08-06 22:16:17 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -119,7 +119,8 @@
             ["sound", "Utw\u00f3r, kt\u00f3ry pu\u015bci\u0142e\u015b ma s\u0142ab\u0105 jako\u015b\u0107 d\u017awi\u0119ku. "],
             ["earrape", "Utw\u00f3r, kt\u00f3ry pu\u015bci\u0142e\u015b jest earrapem. "],
             ["niedostepne", "Utw\u00f3r, kt\u00f3ry pu\u015bci\u0142e\u015b jest niedost\u0119pny dla niekt\u00f3rych u\u017cytkownik\u00f3w. "],
-            ["zadlugie", "Utw\u00f3r, kt\u00f3ry pu\u015bci\u0142e\u015b jest zbyt d\u0142ugi, potrzebujesz pozwolenia na puszczanie utwor\u00f3w d\u0142u\u017cszych ni\u017c 6 minut. "]
+            ["zadlugie", "Utw\u00f3r, kt\u00f3ry pu\u015bci\u0142e\u015b jest zbyt d\u0142ugi, potrzebujesz pozwolenia na puszczanie utwor\u00f3w d\u0142u\u017cszych ni\u017c 6 minut. "],
+            ["meh", "Utw\u00f3r, kt\u00f3ry pu\u015bci\u0142e\u015b otrzyma\u0142 ponad 70% meh\u00f3w. "]
             ],
         afkpositionCheck: 15,
         afkRankCheck: "user",
