187189c9b4b8d7c492295c27372ebd885272a6fa
Hasztagg <a.jurkiewicz5@gmail.com>
2015-08-18 20:39:33 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -80,15 +80,15 @@
             }
         };
         
-        bot.commands.teczkaCommand = { 
-            command: 'teczka',
+        bot.commands.rcsCommand = { 
+            command: 'rcs',
             rank: 'user', 
             type: 'exact', 
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0); 
                 if (!bot.commands.executable(this.rank, chat)) return void(0); 
                 else {
-                    API.sendChat("/me :information_source: Wpisz si\u0119 do teczki inwigilacji (wymaga do\u0142\u0105czenia do grupy) https://www.facebook.com/groups/beczkownicy.plugdj/permalink/512312038933480/!); //wysyla wiadomosc
+                    API.sendChat("/me :information_source: Wpisz si\u0119 do teczki inwigilacji (wymaga do\u0142\u0105czenia do grupy) https://www.facebook.com/groups/beczkownicy.plugdj/permalink/512312038933480/!"); 
 
                 }
             }
