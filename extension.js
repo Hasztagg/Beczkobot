df7dea397079bc1ac2d62c1e39f6c64b0e6bcc2d
Hasztagg <a.jurkiewicz5@gmail.com>
2015-08-22 21:42:23 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -94,6 +94,20 @@
                 }
             }
         };
+        
+        bot.commands.motywCommand = { 
+            command: 'motyw',
+            rank: 'user', 
+            type: 'exact', 
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0); 
+                if (!bot.commands.executable(this.rank, chat)) return void(0); 
+                else {
+                    API.sendChat("!motd"); 
+
+                }
+            }
+        };
 /*        
         bot.commands._nightmodeon = {
             command: 'nightmodeon',
