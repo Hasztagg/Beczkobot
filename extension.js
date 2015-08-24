ce5a9cb46ed17b5d5748185559f9afe4a73f6e90
WorstUdyrDE <WorstUdyrDE@users.noreply.github.com>
2015-08-24 16:29:24 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -133,7 +133,8 @@
                 for (var i = 0; i < deletedWords.length; i++) {
                 if (data.message.toLowerCase().indexOf(deletedWords[i]) > -1) {
                     $.ajax({ type: 'DELETE', url: '/_/chat/' + data.cid });
-                        API.sendChat("/me @" + %%USER%% + " Sam jesteś zwis.");
+                        API.sendChat("/me Obrażasz Disa? :wat:");
+                        // żeby z user bylo to jednak raka mam i nie umiem 
                     break;
                 };
             }
