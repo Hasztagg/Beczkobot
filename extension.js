435f81fcb27ee7a249f2dc8cc9c270817ab8c5b3
Hasztagg <a.jurkiewicz5@gmail.com>
2015-08-27 19:09:01 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -140,6 +140,19 @@
             }
         });
         
+        bot.commands.powiedz = {
+            command: 'powiedz',
+            rank: 'manager',
+            type: 'exact',
+            functionality: function(chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
+                if (!bot.commands.executable(this.rank, chat)) return void(0);
+                else {
+                    var msg = chat.message;
+                    API.sendChat(msg)
+                }
+            }
+        };
 
         // Load the chat package again to account for any changes
         bot.loadChat();
