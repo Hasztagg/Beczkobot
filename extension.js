51e9b9830d0a2b602520e367829146da81d49cf5
Hasztagg <a.jurkiewicz5@gmail.com>
2015-08-28 09:46:31 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -164,8 +164,8 @@
     localStorage.setItem("basicBotsettings", JSON.stringify({
         botName: "Beczkobot",
         language: "polish",
-        chatLink: "https://raw.githubusercontent.com/Hasztagg/basicBot-customization/master/lang/pl.json",
-        scriptLink: "https://rawgit.com/Yemasthui/basicBot/master/basicBot.js",
+        chatLink: "https://rawgit.com/Hasztagg/Beczkobot/master/pl.json",
+        scriptLink: "https://rawgit.com/Hasztagg/Beczkobot/master/basicBot.js",
         roomLock: false, // Requires an extension to re-load the script
         startupCap: 1, // 1-200
         startupVolume: 0, // 0-100
