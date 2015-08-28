5120a4e327feb98e3d74a8ab1796689ba8c1b4a6
Hasztagg <a.jurkiewicz5@gmail.com>
2015-08-28 09:45:29 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -231,6 +231,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/Hasztagg/basicBot/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/Hasztagg/Beczkobot/master/basicBot.js", extend);
 
 }).call(this);
